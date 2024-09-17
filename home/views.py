267e8b3506054043b30e18cc7ee619ff2740be00
Sanket Ghodke <sanketghodke@sankets-air.student.wireless.sydney.edu.au>
2024-09-17 15:36:23 +1000

    Dynamic rendering

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from home.models import ResearchPapers
+
+
 # Create your views here.
 def post_list(request):
     papers = ResearchPapers.objects.all()
     return render(request, 'home/post_list.html', {'papers': papers})
+
+
+def post_detail(request, pk):
+    paper = get_object_or_404(ResearchPapers, pk=pk)
+    return render(request, 'home/post_detail.html', {'paper': paper})
