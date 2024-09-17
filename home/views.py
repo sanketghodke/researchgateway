1a7f64647616d73cec6f6f96197fb25a99a7c516
Sanket Ghodke <sanketghodke@sankets-air.student.wireless.sydney.edu.au>
2024-09-17 14:40:21 +1000

    Added template

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from home.models import ResearchPapers
 # Create your views here.
+def post_list(request):
+    papers = ResearchPapers.objects.all()
+    return render(request, 'home/post_list.html', {'papers': papers})
